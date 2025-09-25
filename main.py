60ab6fc5386870d91f665c38f325058e04a99e93
rosakris2022-crypto <rosakris2022@gmail.com>
2025-09-25 22:21:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,35 @@ import random
 
 """
 class PlayerDatabase:
-    def __init__(self, db_name = "players.db"):
+    def __init__(self, db_name="players.db"):
         self.db_name = db_name
+        self.connection = None
+        self.cursor = None
         self.init_db()
-    
+
     def init_db(self):
-        with sqlite3.connect(self.db_name) as conn:
-            cursor = conn.cursor()
-            cursor.execute('''
-                CREATE TABLE IF NOT EXISTS players (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT NOT NULL,
-                    hp REAL NOT NULL,
-                    armor REAL NOT NULL,
-                    attack REAL NOT NULL
-                )
-            ''')
-            cursor.commit()
+        try:
+            self.connection = sqlite3.connect('players.db')
+            self.cursor = self.connection.cursor()
+
+            self.cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS players (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT NOT NULL,
+                        hp REAL NOT NULL,
+                        armor REAL NOT NULL,
+                        attack REAL NOT NULL
+                    )
+                ''')
+            self.connection.commit()
+        except sqlite3.Error as error:
+            print(error)
+
+        def close_db(self):
+            self.connection.close()
+         self.cursor.close()
+
+    def init
 """
 
 #==========================CLASS PLAYER========================#
